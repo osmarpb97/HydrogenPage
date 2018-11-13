ce89130a82b01b26655879d2333ec27174d257b5
osmarpb.97@gmail.com <osmarpb.97@gmail.com>
2018-11-13 10:09:23 -0600

    First Commit

---
+++ fullpageconfig.js
@@ -0,0 +1,17 @@
+new fullpage('#fullpage', {
+    //options
+    anchors:['FirstSection', 'SecondSection'],
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: true,
+    navigationPosition: 'left',
+    css3: true,
+    scrollingSpeed: 1000,
+    navigation: true,
+    slidesNavigation: true,
+    responsiveHeight: 330,
+    dragAndMove: true
+  });
+
+  //métodos
+  fullpage_api.setAllowScrolling(true);
